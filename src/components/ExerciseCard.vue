86f245eb7f9236c4df63733c8d8c20649239546a
mparma <mparma@itba.edu.ar>
2020-10-07 23:55:41 -0300

    Base de la vista para "Creacion de rutinas"

---
+++ src/components/ExerciseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card width="300px" rounded>
+    <v-img
+        src="https://www.t-nation.com/system/publishing/articles/10001096/original/Unconventional-Workout-Abs.jpg?1515713332"
+        height="100px"
+    />
+    <div class="sharkyPurple white--text">
+      <v-card-title class="d-inline-block pa-3">Ejercicio i</v-card-title>
+      <v-card-actions class="d-inline-block float-right pt-2">
+        <v-btn>
+          <v-icon>mdi-trash-can</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+name: "ExerciseCard"
+}
+</script>
+
+<style scoped>
+
+</style>
