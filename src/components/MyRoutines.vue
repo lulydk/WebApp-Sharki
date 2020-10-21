d1338c9655a6766edcaef8ec3369dbb5f36db435
Lucas <lugomez@itba.edu.ar>
2020-10-21 16:36:48 -0300

    Se agrego favoritos y mis rutinas la perfil

---
+++ src/components/MyRoutines.vue
@@ -0,0 +1,26 @@
+<template>
+  <RoutinesBlock name="Mis Rutinas"/>
+</template>
+
+<script>
+import RoutinesBlock from "@/components/RoutinesBlock";
+export default {
+  name: "MyRoutines",
+  components: {
+    RoutinesBlock
+  },
+  props: ['name'],
+  data () {
+
+  }
+}
+</script>
+
+<style scoped>
+.repes {
+  width: 50px;
+}
+#repes {
+  width: 50px;
+}
+</style
