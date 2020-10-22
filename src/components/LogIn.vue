93206ed79a93527b7bd07fc4aa7d9d6a3b51458a
Lucas <lugomez@itba.edu.ar>
2020-10-22 19:56:57 -0300

    Correcciones al registro y pequenos cambios

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,16 +1,6 @@
 <template>
     <v-container>
-      <v-dialog v-model="dialog" max-width="45%">
-        <template v-slot:activator="{on}">
-          <v-btn
-              color="red lighten-2"
-              dark
-              v-on="on"
-          >
-            NO SE COMO PONER PARA QUE SALTE AL INICIAR. TOCA Y MIRALO
-          </v-btn>
-        </template>
-
+      <v-dialog v-model="dialog" persistent max-width="45%">
         <v-card v-on="on">
           <v-card-title  class="sharkyPurple white--text font-weight-bold">Inicio Sesion</v-card-title>
           <v-text-field class="pa-md-4 mx-lg-auto"
@@ -25,10 +15,21 @@
               label="Contraseña"
               @click:append="show1 = !show1"
           ></v-text-field>
-          <div class="d-flex justify-center pa-md-4" >
-            <v-btn class="pa-md-4 sharkyPurple white--text" depressed large>
-              Iniciar sesion
-            </v-btn>
+          <div class="d-flex justify-center ">
+              <v-btn class="sharkyPurple white--text" depressed large>Iniciar sesion</v-btn>
+          </div>
+          <div class="d-flex justify-center py-md-4">
+            <v-dialog v-model="dialog2" max-width="45%">
+              <template v-slot:activator="{on}">
+                <v-btn class="sharkyLight white--text"
+                       depressed small
+                       @click="openRegister()"
+                       v-on="on"
+                >Registrate
+                </v-btn>
+              </template>
+              <Register/>
+            </v-dialog>
           </div>
         </v-card>
         <v-spacer/>
@@ -37,8 +38,25 @@
 </template>
 
 <script>
+import Register from "../components/Register.vue";
+
 export default {
-  name: "LogIn"
+  name: "LogIn",
+  components: {
+    Register
+  },
+  data(){
+    return {
+      dialog: true,
+      dialog2: false
+    }
+  },
+  methods:{
+    openRegister(){
+      this.dialog = false
+      this.dialog2 = true
+    }
+  }
 }
 </script>
 
