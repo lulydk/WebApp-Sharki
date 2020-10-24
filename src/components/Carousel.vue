2a5258513062c035bc9899fd24d001a7159b9643
lulydk <lucidiaz@itba.edu.ar>
2020-10-24 15:21:37 -0300

    Vista home

---
+++ src/components/Carousel.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-carousel cycle height="300" hide-delimiter-background show-arrows-on-hover>
+        <v-carousel-item v-for="(rout, i) in routines" :key="i">
+            <v-sheet color="sharkyLight" height="100%">
+                <v-img height="300" :src="rout.image" gradient="to left, rgba(90,0,224,0.5), #9752FF">
+                    <v-col class="mt-5 ml-5">
+                        <span class="display-1 font-weight-semibold">{{ rout.name }}</span>
+                    </v-col>
+                    <v-col class="ml-5">
+                        <v-icon small left>mdi-flash</v-icon>
+                        <span class="font-weight-bold">Dificultad: </span>{{ rout.difficulty }}
+                    </v-col>
+                    <v-col cols="8" class="ml-5">
+                        <v-icon small left>mdi-information-outline</v-icon>
+                        <span class="font-weight-bold">Descripción: </span>{{ rout.description }}
+                    </v-col>
+                </v-img>
+            </v-sheet>
+        </v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+export default {
+    props: {
+        routines: Array,
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,48 @@
 <template>
-  <v-container mb-12 fluid>
-    <v-layout class="mx-auto" row wrap>
-      <LogIn/>
-      <v-flex xs12 md12>
-        <h2 class="mb-5">Rutinas Populares</h2>
+  <div>
+    <LogIn/>
+    <div class="create mx-8 mb-4">
+      <!--Heading-->
+      <div class="mb-4">
+        <h1 class="d-inline-block sharkyPurple--text">
+          <v-icon class="pb-2 sharkyPurple--text" large>mdi-home</v-icon>
+          Bienvenido, user
+        </h1>
+      </div>
+      <Carousel :routines=routines />
+      <v-flex v-for="cat in categories" :key="cat.id" class="mt-5">
+        <h3 class="d-inline-block mb-3">Categoría: {{ cat.name }}</h3>
+        <v-slide-group show-arrows class="sharkyBack py-2">
+          <!--CAMBIAR ROUTINES POR LAS RUTINAS DE CIERTA CATEGORIA-->
+          <v-slide-item v-for="r in routines" :key="r.id">
+            <RoutineCard :routine="r"/>
+          </v-slide-item>
+        </v-slide-group>
       </v-flex>
-      <v-flex class="mb-6" v-for="routine in routines" :key="routine.name">
-        <RoutineCard :routine="routine"/>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import RoutineCard from "../components/RoutineCard";
-import LogIn from "../components/LogIn";
+  import RoutineCard from "../components/RoutineCard";
+  import LogIn from "../components/LogIn";
+  import Carousel from "../components/Carousel";
 
 export default {
   name: 'Home',
   components: {
     RoutineCard,
-    LogIn
+    LogIn,
+    Carousel
   },
   data () {
     return {
+      categories: [
+        { name:'Yoga', id:1 },
+        { name:'Musculacion', id:2 },
+        { name:'Cardio', id:3 } 
+      ],
       routines: [
         {   image: 'https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-1.2.1&auto=format&fit=crop&w=794&q=80',
             name: 'Rutina 1',
