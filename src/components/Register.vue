f16392ca9148aa815b0d97d0633a0e479fe1339a
lulydk <lucidiaz@itba.edu.ar>
2020-10-24 14:32:27 -0300

    Login + Register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -8,20 +8,21 @@
         </v-flex>
         <span class="sharkyPurple--text mx-auto mt-2 font-weight-bold">Registrarse</span>
       </v-layout>
+      <v-form class="px-10">
       <v-text-field v-model="input.username"
                     :rules="rules2"
-                    class="px-md-4 pt-md-4 mx-lg-auto"
+                    class="pt-md-4 mx-lg-auto"
                     color="sharkyPurple"
                     label="Usuario"
       ></v-text-field>
       <v-text-field v-model="input.fullName"
                     :rules="rules"
-                    class="px-md-4 mx-lg-auto"
+                    class="mx-lg-auto"
                     label="Nombre completo"
       ></v-text-field>
       <v-text-field v-model="input.email"
                     :rules="emailRules"
-                    class="pa-md-4 mx-lg-auto"
+                    class="mx-lg-auto"
                     color="sharkyPurple"
                     label="Mail"
       ></v-text-field>
@@ -30,7 +31,7 @@
                     :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
                     :rules="rules2"
                     :type="show1 ? 'text' : 'password'"
-                    class="px-md-4 mx-lg-auto"
+                    class="mx-lg-auto"
                     color="sharkyPurple"
                     label="Contraseña"
                     @click:append="show1 = !show1"
@@ -39,7 +40,7 @@
                     :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
                     :rules="rules2"
                     :type="show1 ? 'text' : 'password'"
-                    class="px-md-4 mx-lg-auto"
+                    class="mx-lg-auto"
                     color="sharkyPurple"
                     label="Repetir contraseña"
                     @click:append="show1 = !show1"
@@ -51,9 +52,10 @@
       <v-card-text v-if="incorrectPass" class="red--text" align="center">
         Las contraseñas no son iguales
       </v-card-text>
+      </v-form>
 
-      <div class="d-flex justify-center pa-md-4">
-        <v-btn class="pa-md-4 sharkyPurple white--text"
+      <div class="d-flex justify-center pa-4">
+        <v-btn class="pa-4 sharkyPurple white--text"
                depressed large
                @click="tryRegister()"
         >
